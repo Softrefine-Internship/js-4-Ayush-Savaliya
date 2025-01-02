b5c15fc70e6e22ca033e0c9d9b7f4953e813991c
Ayush Savaliya <ayushsavaliya129@gmail.com>
2025-01-02 20:03:51 +0530

    Completed pop-up box for add expenses

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 // write javascript here
+
+const popup = document.querySelector(".addexpense");
+    // const overlay = document.querySelector(".overlay");
+    const btnClosePopup = document.querySelector(".close-popup");
+const btnsOpenPopup = document.querySelector(".show-popup");
+    const btnExpense = document.querySelector(".btn--expense");
+
+    const openpopup = function () {
+      popup.classList.remove("hidden");
+      overlay.classList.remove("hidden");
+    };
+
+    const closepopup = function () {
+      popup.classList.add("hidden");
+      overlay.classList.add("hidden");
+    };
+
+
+
+btnsOpenPopup.addEventListener("click", openpopup);
+btnClosePopup.addEventListener("click", closepopup);
+// overlay.addEventListener("click", closepopup);
+// btnExpense.addEventListener("click", closepopup);
+
+
+btnExpense.addEventListener("click", function () {
+  // alert("saugh");  
+  closepopup(); 
+});
+    
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !popup.classList.contains("hidden")) {
+    closepopup();
+  }
+});
+    
+
